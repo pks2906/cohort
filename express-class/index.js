4769cba62d22c7ae3a75230baac007407323605f
pks2906 <pratik1971sinha@gmail.com>
2024-07-28 12:33:22 +0530

    simple task

---
+++ express-class/index.js
@@ -0,0 +1,28 @@
+// creating a HTTP server
+// server
+// node default library -> no
+
+const express  = require("express");
+
+
+
+const app = express();
+
+function sum(n) {
+    let ans = 0;
+    for (let i = 1; i<=n; i++) {
+        ans = ans + i;
+    }
+
+    return ans;
+}
+
+app.get("/", function(req, res) {
+    const n = req.query.n;
+    const ans = sum(n);
+    res.send("hi your ans is " +  ans);   
+})
+
+app.listen(3000);   
+
+//simple task 
