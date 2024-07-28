40893b7e3be044ef5c43335cd1f2b161bc17e854
pks2906 <pratik1971sinha@gmail.com>
2024-07-28 13:16:20 +0530

    filter in js

--- express-class/index.js
+++ express-class/index.js
@@ -2,27 +2,59 @@
 // server
 // node default library -> no
 
+// const express  = require("express");
+
+
+
+// const app = express();
+
+// function sum(n) {
+//     let ans = 0;
+//     for (let i = 1; i<=n; i++) {
+//         ans = ans + i;
+//     }
+
+//     return ans;
+// }
+
+// app.get("/", function(req, res) {
+//     // const n = req.query.n;
+//     // const ans = sum(n);
+//     // res.send("hi your ans is " +  ans);   
+// })
+
+// app.listen(3000);   
+
 const express  = require("express");
-
-
-
 const app = express();
 
-function sum(n) {
-    let ans = 0;
-    for (let i = 1; i<=n; i++) {
-        ans = ans + i;
-    }
 
-    return ans;
-}
-
+const users = [{
+    name: "john",
+    kidneys: [{
+        healthy: false
+    }]
+}];
+// filter in js
 app.get("/", function(req, res) {
-    const n = req.query.n;
-    const ans = sum(n);
-    res.send("hi your ans is " +  ans);   
+    //write logic
+    const johnKidneys = users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for (let i = 0; i<johnKidneys.length; i++) {
+
+        if (johnKidneys[i].healthy){
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;   
+        }
+    }
+    const numberOfUnHealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnHealthyKidneys
+    })
+
 })
 
-app.listen(3000);   
 
-//simple task done 
+app.listen(3000);
