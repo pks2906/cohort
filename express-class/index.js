30cc36328c22b539288131f7e7dbf3d5027340d2
pks2906 <pratik1971sinha@gmail.com>
2024-08-01 22:31:12 +0530

    auth and input validation

--- express-class/index.js
+++ express-class/index.js
@@ -25,44 +25,72 @@
 
 // app.listen(3000);   
 
-const express  = require("express");
+// const express  = require("express");
+// const app = express();
+
+
+// const users = [{
+//     name: "john",
+//     kidneys: [{
+//         healthy: false
+//     }]
+// }];
+// // filter in js
+// app.get("/", function(req, res) {
+//     //write logic
+//     const johnKidneys = users[0].kidneys;
+//     const numberOfKidneys = johnKidneys.length;
+//     let numberOfHealthyKidneys = 0;
+//     for (let i = 0; i<johnKidneys.length; i++) {
+
+//         if (johnKidneys[i].healthy){
+//             numberOfHealthyKidneys = numberOfHealthyKidneys + 1;   
+//         }
+//     }
+//     const numberOfUnHealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+//     res.json({
+//         numberOfKidneys,
+//         numberOfHealthyKidneys,
+//         numberOfUnHealthyKidneys
+//     })
+
+// })
+
+// app.post("/", function(req, res){
+//     const isHealthy = req.body.isHealthy;
+//     users[0].kidneys.push({
+//         healthy: isHealthy
+//     })
+//     res.json({
+//         msg: "Done!"
+//     })
+// })
+// app.listen(3000);
+
+const express = require("express");
+
 const app = express();
 
+app.get("/health-checkup", function (res, req) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+    const kidneyId = req.query.kidneyId;
 
-const users = [{
-    name: "john",
-    kidneys: [{
-        healthy: false
-    }]
-}];
-// filter in js
-app.get("/", function(req, res) {
-    //write logic
-    const johnKidneys = users[0].kidneys;
-    const numberOfKidneys = johnKidneys.length;
-    let numberOfHealthyKidneys = 0;
-    for (let i = 0; i<johnKidneys.length; i++) {
+    if (username === "Pratik" || password === "pass") {
+        if (kidneyId == 1 || kidneyId == 2) {
+            //do something with kidney here
+            res.json({
+                msg: "kidney is fine!"
+            })
 
-        if (johnKidneys[i].healthy){
-            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;   
         }
+        
+        
+
+        
     }
-    const numberOfUnHealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
-    res.json({
-        numberOfKidneys,
-        numberOfHealthyKidneys,
-        numberOfUnHealthyKidneys
-    })
+    res.status(400).json({"msg": "Somethings up with your inputs"})
 
-})
+});
 
-app.post("/", function(req, res){
-    const isHealthy = req.body.isHealthy;
-    users[0].kidneys.push({
-        healthy: isHealthy
-    })
-    res.json({
-        msg: "Done!"
-    })
-})
 app.listen(3000);
