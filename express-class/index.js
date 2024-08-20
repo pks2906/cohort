ddd116bc223d492442edbaaebe629ba59cea2b83
pks2906 <pratik1971sinha@gmail.com>
2024-08-20 22:51:32 +0530

    ugly way

--- express-class/index.js
+++ express-class/index.js
@@ -71,26 +71,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/health-checkup", function (res, req) {
-    const username = req.headers.username;
-    const password = req.headers.password;
-    const kidneyId = req.query.kidneyId;
+app.get("/health-checkup", function (req, res){
+  const username = req.headers.username;
+  const password = req.headers.password;
+  const kidneyId = req.query.kidneyId;
 
-    if (username === "Pratik" || password === "pass") {
-        if (kidneyId == 1 || kidneyId == 2) {
-            //do something with kidney here
-            res.json({
-                msg: "kidney is fine!"
-            })
-
-        }
-        
-        
-
-        
+  if(username === "pratik" && password === "pass") {
+    if (kidneyId == 1 || kidneyId == 2){
+      // do something with kidney here
+      res.json({
+        msg: "Your kidney is healthy!"
+      })
+      
+    } else {
+      
     }
-    res.status(400).json({"msg": "Somethings up with your inputs"})
-
+    
+    
+  }
+  res.status(400).json({"msg": "Somethings up with your inputs"})
 });
 
 app.listen(3000);
