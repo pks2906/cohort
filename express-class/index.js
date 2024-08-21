48dc770cc03b8bde6486b1abb9dcad319ebeaaf5
pks2906 <pratik1971sinha@gmail.com>
2024-08-21 23:06:09 +0530

    zod for i/p validation

--- express-class/index.js
+++ express-class/index.js
@@ -148,6 +148,21 @@ const app = express();
 
 const schema = zod.array(zod.number());
 
+// 
+// {
+//  email: string => email,  
+//  password: atleast 8 letters,
+//  country: "IN" or "US"
+// }
+//
+
+const schema = zod.object({
+  email: zod.string(),
+  password: z.string(),
+  country: z.literal("IN").or(z.literal("US")),
+  kidneys: z.array(z.number())  
+})
+
 app.use(express.json());
 
 app.post("/health-checkup", function (req, res) {
